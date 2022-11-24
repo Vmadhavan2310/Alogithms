ba21b03ffb02b43457a8ef703a24253d2268e915
Vmadhavan2310 <105116989+Vmadhavan2310@users.noreply.github.com>
2022-11-24 12:08:58 +0530

    Create patterns.js

---
+++ patterns.js
@@ -0,0 +1,19 @@
+/* Square pattern
+*****
+*****
+*****
+*****
+*****
+*/
+
+function pattern(n) {
+  let value = '*'
+  for (let i = 0; i < n; i++) {
+    for (let j = 0; j < n; j++) {
+      value+='*'
+    }
+    console.log(value)
+    value='*'
+  }
+}
+pattern(10)
