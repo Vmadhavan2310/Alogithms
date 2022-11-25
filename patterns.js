38efd269fcc5cbdfbb5430be75e240dab1b33d49
Vmadhavan2310 <105116989+Vmadhavan2310@users.noreply.github.com>
2022-11-25 09:06:36 +0530

    Update patterns.js

--- patterns.js
+++ patterns.js
@@ -17,3 +17,25 @@ function squarePattern(n) {
   }
 }
 pattern(10)
+
+//pyramidPattern
+    *
+   ***
+  *****
+ *******
+*********
+  function pyramidPattern(n){
+  let value=''
+  for(let i=1;i<=n;i++){
+    for(let j=0;j<n-i;j++){
+      value+=' '
+    }
+    for(let k=0;k<2*i-1;k++){
+      value+='*'
+    }
+    value+='\n'
+  }
+  return value;
+}
+
+console.log(pyramidPattern(5))
